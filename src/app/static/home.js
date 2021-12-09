4c9eacb310014a6ef618b5b977718a9473be5759
kopokopok <michaelzhou7@gmail.com>
2021-12-09 05:21:16 -0500

    https compatibility

--- src/app/static/home.js
+++ src/app/static/home.js
@@ -13,7 +13,7 @@ $(document).ready(() => {
   }).resize();
 });
 
-const socket = new WebSocket(`ws://${window.location.hostname}:${window.location.port}`);
+const socket = new WebSocket(`wss://${window.location.hostname}:${window.location.port}`);
 socket.addEventListener("open", () => {
   console.log("Connected to WS server");
 
